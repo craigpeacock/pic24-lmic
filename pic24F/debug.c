f5e386c200c4f1c41b3cb3c0e4c5606b7a00170e
Craig Peacock <Craig.Peacock.2013@beyondlogic.org>
2018-05-10 22:19:06 +0930

    Add empty pic24F processor specific files.

---
+++ pic24F/debug.c
@@ -0,0 +1,37 @@
+#include <oslmic.h>
+
+void debug_init () {
+	
+}
+
+void debug_led (u1_t val) {
+
+}
+
+void debug_char (u1_t c) {
+
+}
+
+void debug_hex (u1_t b) {
+
+}
+
+void debug_buf (const u1_t* buf, u2_t len) {
+
+}
+
+void debug_uint (u4_t v) {
+
+}
+
+void debug_str (const u1_t* str) {
+	
+}
+
+void debug_event (int ev) {
+
+}
+
+void debug_val (const u1_t* label, u4_t val) {
+	
+}
---
+++ pic24F/hal.c
@@ -0,0 +1,54 @@
+#include <oslmic.h>
+#include <lorabase.h>
+
+void hal_init () {
+	
+}
+
+void hal_failed () {
+
+}
+
+void hal_pin_rxtx (u1_t val) {
+
+}
+
+void hal_pin_nss (u1_t val) {
+
+}
+
+void hal_pin_rst (u1_t val) {
+	
+}
+
+//void radio_irq_handler (u1_t dio) {
+//}
+
+u1_t hal_spi (u1_t outval) {
+
+}
+
+u4_t hal_ticks () {
+
+}
+
+void hal_waitUntil (u4_t time) {
+
+}
+
+u1_t hal_checkTimer (u4_t targettime) {
+
+}
+
+void hal_disableIRQs () {
+
+}
+
+void hal_enableIRQs () {
+
+}
+
+void hal_sleep () {
+	
+}
+
